ccd0daa7bfe988820779c1b24d8db5d597f1b43d
Jason Featheringham <thejase@gmail.com>
2018-01-20 21:41:30 -0800

    15 - use search-bar

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { YOUTUBE_API_KEY } from '../api-keys';
+import SearchBar from './components/search-bar';
+
+console.log(YOUTUBE_API_KEY);
 
 // Create a new component.  Should produce some HTML
-const App = () => <div>Hi!</div>;
+const App = () => (
+  <div>
+    <SearchBar />
+  </div>
+);
 // take component and put on the page
 ReactDOM.render(<App />, document.getElementById('root'));
