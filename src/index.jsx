a6b033b0666856433941c736367a9700c4cd14a7
Jason Featheringham <thejase@gmail.com>
2018-01-20 23:15:32 -0800

    22 - add app videos state

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import { YOUTUBE_API_KEY } from '../api-keys';
 import SearchBar from './components/search-bar';
 
 // Create a new component.  Should produce some HTML
-const App = () => (
-  <div>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+    };
+
+    YTSearch(
+      { key: YOUTUBE_API_KEY, term: 'surfboards' },
+      (videos) => { this.setState({ videos }); },
+    );
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 // take component and put on the page
 ReactDOM.render(<App />, document.getElementById('root'));
