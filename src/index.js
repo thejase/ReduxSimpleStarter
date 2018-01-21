411afd49baa7639a186135fa8540d88e3c63eb2e
Jason Featheringham <thejase@gmail.com>
2018-01-20 19:40:23 -0800

    11 - app render

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // Create a new component.  Should produce some HTML
 const App = () => <div>Hi!</div>;
 // take component and put on the page
+ReactDOM.render(<App />, document.getElementById('root'));
