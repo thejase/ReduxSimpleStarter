4180c83484d2e422fd1e1fda665c4b8beb5266af
Jason Featheringham <thejase@gmail.com>
2018-01-20 21:59:29 -0800

    16 - search-bar as class

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleInputChange(e) {
+    const { target: { value } } = e;
+    console.log({ value });
+  }
+
   render() {
-    return <input />;
+    return <input onChange={this.handleInputChange} />;
   }
 }
 
