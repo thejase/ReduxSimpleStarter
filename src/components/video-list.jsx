00dfafe3f74e3487d39a30dbdbae3119f9801b33
Jason Featheringham <thejase@gmail.com>
2018-01-22 01:20:38 -0800

    26 - VideoListItem

--- src/components/video-list.jsx
+++ src/components/video-list.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import VideoListItem from './video-list-item';
 
 const VideoList = ({
   videos,
 }) => {
   return (
-    <ul className="col-md-4 list-group">
-      {videos.length}
+    <ul className="col-4 list-group">
+      {videos.map(video => <VideoListItem key={video.etag} video={video} />)}
     </ul>
   );
 }
